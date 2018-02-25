14bfc9dce2e37a65cf71d324e635aaad762daa40
Syc <syc@bilibili.de>
2018-02-25 12:12:01 +0800

    Update L1-038

--- L1/L1-038.c
+++ L1/L1-038.c
@@ -5,3 +5,11 @@
  * Github: https://github.com/SycAlright/PAT-GPLT
  * Blog: https://www.mfeng.cc/archives/2018/02/01/PAT-GPLT.html
  */
+
+#include <stdio.h>
+int main()
+{
+	printf("Hello World\n"); 
+	printf("Hello New World");
+	return 0;
+}
