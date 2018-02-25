d2ec24353f5a4a6aecf0f1b266bee876d5dd53a8
Syc <syc@bilibili.de>
2018-02-25 11:58:35 +0800

    Update L1-034

--- L1/L1-034.c
+++ L1/L1-034.c
@@ -5,3 +5,32 @@
  * Github: https://github.com/SycAlright/PAT-GPLT
  * Blog: https://www.mfeng.cc/archives/2018/02/01/PAT-GPLT.html
  */
+
+#include <stdio.h>
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	int arr[1001] = {0};
+	int i, j, K, num;
+	for (i=0; i<N; i++)
+	{
+		scanf("%d", &K);
+		for (j=0; j<K; j++)
+		{
+			scanf("%d", &num);
+			arr[num]++;
+		}
+	}
+	int maxIndex=0, maxValue=0;
+	for (i=0; i<1001; i++)
+	{
+		if (arr[i]>=maxValue)
+		{
+			maxValue=arr[i];
+			maxIndex=i;
+		}
+	}
+	printf("%d %d", maxIndex, maxValue);
+	return 0;
+}
