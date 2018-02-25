0baedb4525ac5825f0956b648ca0c67e272792a3
Syc <syc@bilibili.de>
2018-02-25 12:12:10 +0800

    Update L1-039

--- L1/L1-039.c
+++ L1/L1-039.c
@@ -5,3 +5,32 @@
  * Github: https://github.com/SycAlright/PAT-GPLT
  * Blog: https://www.mfeng.cc/archives/2018/02/01/PAT-GPLT.html
  */
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	int N, m;
+	char s[1001], a[101][1001];
+	scanf("%d", &N);
+	getchar(); //防止回车被Gets 
+	gets(s);
+	int l=strlen(s), t=0;
+	m=(l+N-1)/N; //和下面两行一样的效果
+	int i, j;
+	for(j=m-1; j>=0; j--)
+	{
+		for(i=0; i<N; i++)
+		{
+			if(t<l) a[i][j]=s[t++];
+			else a[i][j]=' '; //不足的补空格
+		}
+	}
+	for(j=0; j<N; j++)
+	{
+		for(i=0; i<m; i++)
+			printf("%c",a[j][i]);
+		printf("\n");
+	}
+	return 0;
+}
